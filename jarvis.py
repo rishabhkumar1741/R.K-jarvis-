4c82199f2fe63a03465a55698b8a582e2db46405
Rishabh Kumar <1741rishabh@hmail.com>
2021-01-19 09:43:27 +0530

    add take_command function and now workin on a calculator

--- jarvis.py
+++ jarvis.py
@@ -1,15 +1,15 @@
 import pyttsx3
 import datetime
-
-engine = pyttsx3.init()
-engine.setProperty('rate',130)
-
+import speech_recognition as sr
+import pyaudio
 
 class Jarvis():
     def __init__(self):
-        pass
+        pass      
 
     def speak(self,audio):
+        engine = pyttsx3.init()
+        engine.setProperty('rate',180)
         engine.say(audio)
         engine.runAndWait()
     
@@ -21,12 +21,49 @@ class Jarvis():
             self.speak("Good Afternoon Sir")
         else:
             self.speak("Good Night Sir")
+        self.speak("I am Jarvis. please tell me how may i help you")
+    
+    def take_command(self):
+        r = sr.Recognizer() 
+      
+        with sr.Microphone() as source :
+            print("Listening.......")
+            r.pause_threshold = 1
+            audio = r.listen(source) 
+        try:
+            message = r.recognize_google(audio,language='en-in') 
+            return message   
+        except Exception as e:
+            print("TRY again ")
+            return "None"
+
+    def sum(self):
+        self.speak("sum of two number is 10")
+
+    
+
 
 if __name__ == "__main__":
     obj = Jarvis()
     obj.wish()
-    
-    
+    message = obj.take_command()
+    message =message.split(' ')
+    print(message)
+    for x in message:
+        if x == "addition":
+            print("sum function")
+            break
+        elif x =="subtract":
+            print("sub")
+            break
+        elif x =="multiply":
+            print("mul")
+            break
+        else:
+            print("not under stant")
+
+
+
 
                   
 
